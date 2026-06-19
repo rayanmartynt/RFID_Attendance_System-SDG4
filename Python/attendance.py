c525a5069646934577db4b25f1e2d7bb91e29cb7
rayanmartynt <rayanmartynt@gmail.com>
2026-06-19 18:36:35 +0000

    Added every block of code in a specified function

--- Python/attendance.py
+++ Python/attendance.py
@@ -5,65 +5,100 @@ import time
 # Arduino setup
 PORT = "COM3"
 BAUD = 9600
-# FILE = "Attendance.xlsx"
+FILE = "Attendance.xlsx"
 
 # Arduino Connection
-try:
-    arduino = serial.Serial(PORT, BAUD, timeout = 1)
-    time.sleep(2)
-    print("Connected to Arduino")
-except:
-    print("Could not connect to Arduino")
-    exit()
-
-print("RFID Attendance System Started")
-while True:
+def connect_arduino():
     try:
-        current_week = int(input("Enter Current week (1-12):"))
-        if 1 <= current_week <= 12:
-            break
-        else:
+        arduino = serial.Serial(PORT, BAUD, timeout = 1)
+        time.sleep(2)
+        print("Connected to Arduino")
+        return arduino
+    except Exception as e:
+        print("Could not connect to Arduino")
+        return None
+
+def select_week():
+
+    while True:
+        try:
+            current_week = int(input("Enter Current week (1-12):"))
+            if 1 <= current_week <= 12:
+                return f"Week {current_week}"
             print("Week number must be between 1 and 12")
-    except ValueError:
-        print("Please enter a valid integer.")
-CURRENT_WEEK = f"Week {current_week}"
-print(f"Recording attendance for {CURRENT_WEEK}")
+        except ValueError:
+            print("Please enter a valid integer.")
 
-while True:
-    try:
-        uid = arduino.readline().decode().strip().upper()
+def load_attendance_database():
+    df = pd.read_excel(FILE)
+    df["RFID_UID"] = (df["RFID_UID"].astype(str).str.upper())
+    return df
+
+def find_student(df, uid):
+    student = df[df["RFID_UID"] == uid]
+    if student.empty:
+        return None
+
+    return student
+
+def save_attendance(df):
+    df.to_excel(
+        FILE,
+        index = False
+    )
+
+def mark_attendance(df, uid, current_week):
+    df.loc[
+        df["RFID_UID"] == uid,
+        current_week
+    ] = 1
+    save_attendance(df)
+
+def send_success_message(arduino, student_name):
+    print(f"{student_name} Attendance recorded")
+    arduino.write(f"SUCCESS:{student_name}\n".encode())
+
+def send_duplicate_message(arduino, student_name):
+    print(f"{student_name} Already Present")
+    arduino.write(f"DUPLICATE:{student_name}\n".encode())
+
+def send_unknown_message(arduino):
+    print("Unknown Card")
+    arduino.write(b"NOTFOUND\n")
+
+def process_card(arduino, uid, current_week):
+    df = load_attendance_database()
+
+    student = find_student(df, uid)
+    if student is None:
+        send_unknown_message(arduino)
+        return
+    student_name = (student["Name"].values[0])
+
+    attendance_status = (student["Attendance"].values[0])
+    if attendance_status == 1:
+        send_duplicate_message(arduino, student_name)
+    else:
+        mark_attendance(df, uid, current_week)
+        send_success_message(arduino, student_name)
+
+def main():
+    arduino = connect_arduino()
+
+    if arduino is None:
+        return
+
+    current_week = select_week()
+    print(f"Recording attendance for week {current_week}")
+
+    while True:
+        uid = (arduino.readline().decode().strip().upper())
 
         if uid == "":
+            print("Scan your card...")
             continue
-        print(f"Card Scanned:{uid}")
+        print(f"Scanned UID: {uid}")
+        process_card(arduino, uid, current_week)
 
-        df = pd.read_excel("Attendance.xlsx")
-        df["RFID_UID"].astype(str).str.upper()
-
-        # Card not registered
-        if uid not in df["RFID_UID"].values:
-            print("Unknow RFID Card")
-            arduino.write(b"NOTFOUND\n")
-            continue
-
-        # Get student record
-        student = df[df["RFID_UID"] == uid]
-
-        name = student["Name"].values[0]
-        attendance = student[CURRENT_WEEK].values[0]
-
-        # Already present
-        if attendance == 1:
-            print(f"{name} already present")
-            arduino.write(f"DUPLICATE:{name}\n".encode())
-        else:
-            df.loc[
-                df["RFID_UID"] == uid,
-                CURRENT_WEEK
-            ] = 1
-
-            df.to_excel("Attendance.xlsx", index = False)
-            print(f"{name} attendance marked")
-            arduino.write(f"SUCCESS:{name}\n".encode())
-    except Exception as e:
-        print("Error:", e)
+if __name__ == "__main__":
+    main()
