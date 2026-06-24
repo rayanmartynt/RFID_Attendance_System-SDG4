8811bbb2f1bf2e0a67aadb0deda5a7c5f0532e18
rayanmartynt <rayanmartynt@gmail.com>
2026-06-24 07:13:49 +0000

    Add the process card function

--- Python/attendance.py
+++ Python/attendance.py
@@ -49,6 +49,87 @@ def get_session_times(session):
     end_time = datetime.strptime(end_time,"%H:%M")
     return start_time, end_time
 
+# Function 7: Process card step by step
+def process_card(arduino, uid, current_week, current_session):
+    df = load_attendance_database()
+    student = find_student(df, uid)
+
+    # Unknown Card
+    if student.empty:
+        print("Unknown Card")
+        send_message(arduino,"NOTFOUND")
+        return
+    row_index = student.index[0]
+    student_name = df.loc[row_index, "Name"]
+    arrival_col = f"Week{current_week}_Arrival"
+    departure_col = f"Week{current_week}_Departure"
+    status_col = f"Week{current_week}_Status"
+
+    now = datetime.now()
+
+    arrival = df.loc[row_index, arrival_col]
+
+    departure = df.loc[row_index, departure_col]
+
+    session_start, session_end = get_session_times(current_session)
+
+    session_start = now.replace(
+        hour=session_start.hour,
+        minute=session_start.minute,
+        second=0,
+        microsecond=0
+    )
+
+    session_end = now.replace(
+        hour=session_end.hour,
+        minute=session_end.minute,
+        second=0,
+        microsecond=0
+    )
+
+    # First Scan: Check-in
+    if pd.isna(arrival):
+        arrival_time = now.strftime("%H:%M:%S")
+        df.loc[row_index, arrival_col] = arrival_time
+
+        # If a student is 30mins late it will record "Late"
+        late_limit = session_start + timedelta(minutes=30)
+
+        if now <= late_limit:
+            status = "Present"
+            send_message(arduino,f"PRESENT:{student_name}")
+            print(f"{student_name} Present")
+        else:
+            status = "Late"
+            send_message(arduino,f"LATE:{student_name}")
+            print(f"{student_name} Late")
+
+        df.loc[row_index, status_col] = status
+
+        save_attendance_database(df)
+        return
+
+    # Second Scan: Check-out
+    if pd.isna(departure):
+        departure_time = now.strftime("%H:%M:%S")
+        df.loc[row_index, departure_col] = departure_time
+
+        status = df.loc[row_index,status_col]
+        if now < session_end:
+            status += " - Left Early"
+
+        df.loc[row_index, status_col] = status
+
+        save_attendance_database(df)
+
+        send_message(arduino,f"CHECKOUT:{student_name}")
+        print(f"{student_name} Checked Out")
+        return
+
+    send_message(arduino,"DUPLICATE")
+
+    print(f"{student_name} Already Check-in")
+
 # Function 11: The main function of this project
 def main():
     print("\n===== RFID ATTENDANCE SYSTEM =====\n")
